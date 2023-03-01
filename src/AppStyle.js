8051c2273e509842eedc479c53a980baf2fa02bf
IgorGap <ig.gaponenko@gmail.com>
2023-03-01 09:31:45 -0300

    all done

---
+++ src/AppStyle.js
@@ -0,0 +1,18 @@
+export const containerStyles = {
+  position: 'relative',
+
+  width: '80%',
+  height: '500px',
+  margin: '80px auto',
+}
+export const borderStyles = {
+  border: '5px solid black',
+  borderRadius: '10px',
+  margin: '80px',
+  background: '#92a8d5',
+}
+export const titleStyles = {
+  margin: '50px',
+  display: 'flex',
+  justifyContent: 'center',
+}
